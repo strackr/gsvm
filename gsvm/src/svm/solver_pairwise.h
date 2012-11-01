badb652fcfed40ec7a643f20444c7144bc6b57eb
Robert Strack <robert.strack@gmail.com>
2012-11-01 16:56:51 -0400

    initialization of the pairwise solver

--- gsvm/src/svm/solver_pairwise.h
+++ gsvm/src/svm/solver_pairwise.h
@@ -21,8 +21,28 @@
 
 #include "solver_common.h"
 
+template<typename K, typename V>
+struct PairValueComparator {
+
+  bool operator() (pair<K, V> pair1, pair<K, V> pair2);
+
+};
+
+template<typename K, typename V>
+bool PairValueComparator<K, V>::operator() (pair<K, V> pair1, pair<K, V> pair2) {
+	return pair1.second < pair2.second;
+}
+
+
 template<typename Kernel, typename Matrix, typename Strategy>
 class PairwiseSolver: public AbstractSolver<Kernel, Matrix, Strategy> {
+
+	label_id maxLabel;
+	quantity* labelSizes;
+	label_id* labelOrder;
+
+	void sortLabels(label_id *labels, quantity size,
+			label_id first, label_id second);
 
 public:
 	PairwiseSolver(map<label_id, string> labelNames, Matrix *samples,
@@ -41,10 +61,31 @@ PairwiseSolver<Kernel, Matrix, Strategy>::PairwiseSolver(
 		label_id *labels, TrainParams &params,
 		StopCriterionStrategy *stopStrategy) :
 		AbstractSolver<Kernel, Matrix, Strategy>(labelNames, samples, labels, params, stopStrategy) {
+	maxLabel = labelNames.size();
+	labelSizes = new quantity[maxLabel];
+	labelOrder = new label_id[maxLabel];
+	for (label_id label = 0; label < maxLabel; label++) {
+		labelSizes[label] = 0;
+	}
+	for (sample_id sample = 0; sample < this->size; sample++) {
+		labelSizes[this->labels[sample]]++;
+	}
+	// sort
+	vector<pair<label_id, quantity> > pairs;
+	for (label_id label = 0; label < maxLabel; label++) {
+		pairs[label] = pair<label_id, quantity>(labelOrder[label], labelSizes[label]);
+	}
+	PairValueComparator<label_id, quantity> comparator;
+	sort(pairs.begin(), pairs.end(), comparator);
+	for (label_id label = 0; label < maxLabel; label++) {
+		labelOrder[label] = pairs[maxLabel - label - 1].first;
+	}
 }
 
 template<typename Kernel, typename Matrix, typename Strategy>
 PairwiseSolver<Kernel, Matrix, Strategy>::~PairwiseSolver() {
+	delete [] labelSizes;
+	delete [] labelOrder;
 }
 
 template<typename Kernel, typename Matrix, typename Strategy>
@@ -56,7 +97,33 @@ CrossClassifier<Kernel, Matrix>* PairwiseSolver<Kernel, Matrix, Strategy>::getCl
 template<typename Kernel, typename Matrix, typename Strategy>
 void PairwiseSolver<Kernel, Matrix, Strategy>::train() {
 	// XXX do pairwise training
-	this->trainForCache(this->cache);
+	for (label_id i = 0; i < maxLabel; i++) {
+		label_id label1 = labelOrder[i];
+		for (label_id j = i + 1; j < maxLabel; j++) {
+			label_id label2 = labelOrder[j];
+			sortLabels(this->labels, this->currentSize, label1, label2);
+			this->trainForCache(this->cache);
+			// TODO save results
+		}
+	}
+}
+
+template<typename Kernel, typename Matrix, typename Strategy>
+void PairwiseSolver<Kernel, Matrix, Strategy>::sortLabels(
+		label_id* labels, quantity size, label_id first, label_id second) {
+	id train = 0;
+	id test = size;
+	while (train < test) {
+		while (labels[train] == first || labels[train] == second) {
+			train++;
+		}
+		while (labels[test] != first && labels[test] != second) {
+			test--;
+		}
+		if (train < test) {
+			this->swapSamples(train++, test--);
+		}
+	}
 }
 
 #endif
