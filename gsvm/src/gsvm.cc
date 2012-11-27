9a0e1961bd50b2cf95b5cca1b27ed2ddf79fb07a
Robert Strack <robert.strack@gmail.com>
2012-11-26 20:34:55 -0500

    changed default cache size

--- gsvm/src/gsvm.cc
+++ gsvm/src/gsvm.cc
@@ -40,23 +40,35 @@ int main(int argc, char *argv[]) {
 	options_description desc(usage + descr + options);
 	desc.add_options()
 		(PR_HELP, "produce help message")
-		(PR_C_LOW, value<fvalue>()->default_value(0.0625), "C value (lower bound)")
-		(PR_C_HIGH, value<fvalue>()->default_value(1024), "C value (upper bound)")
-		(PR_G_LOW, value<fvalue>()->default_value(0.0009765625), "gamma value (lower bound)")
-		(PR_G_HIGH, value<fvalue>()->default_value(16), "gamma value (upper bound)")
+		(PR_C_LOW, value<fvalue>()->default_value(0.0625),
+				"C value (lower bound)")
+		(PR_C_HIGH, value<fvalue>()->default_value(1024),
+				"C value (upper bound)")
+		(PR_G_LOW, value<fvalue>()->default_value(0.0009765625),
+				"gamma value (lower bound)")
+		(PR_G_HIGH, value<fvalue>()->default_value(16),
+				"gamma value (upper bound)")
 		(PR_RES, value<int>()->default_value(8), "resolution (for C and gamma)")
 		(PR_OUTER_FLD, value<int>()->default_value(1), "outer folds")
 		(PR_INNER_FLD, value<int>()->default_value(10), "inner folds")
-		(PR_EPSILON, value<string>()->default_value(EPSILON_DEFAULT), "epsilon value")
+		(PR_EPSILON, value<string>()->default_value(EPSILON_DEFAULT),
+				"epsilon value")
 		(PR_ETA, value<fvalue>()->default_value(DEFAULT_ETA), "eta value")
 		(PR_DRAW_NUM, value<int>()->default_value(600), "draw number")
-		(PR_MULTICLASS, value<string>()->default_value(MULTICLASS_ALL_AT_ONCE), "multiclass training approach (allatonce or pairwise)")
-		(PR_SEL_TYPE, value<string>()->default_value(SEL_TYPE_PATTERN), "model selection type (grid or pattern)")
-		(PR_MATRIX_TYPE, value<string>()->default_value(MAT_TYPE_SPARSE), "data representation (sparse or dense)")
-		(PR_STOP_CRIT, value<string>()->default_value(STOP_CRIT_ADJMN), "stopping criterion (adjmn, mn or meb)")
-		(PR_OPTIMIZATION, value<string>()->default_value(OPTIMIZATION_MDM), "optimization strategy (mdm or imdm)")
-		(PR_ID_RANDOMIZER, value<string>()->default_value(ID_RANDOMIZER_FAIR), "id generator (simple, fair or determ)")
-		(PR_CACHE_SIZE, value<int>()->default_value(DEFAULT_CACHE_SIZE), "cache size")
+		(PR_MULTICLASS, value<string>()->default_value(MULTICLASS_ALL_AT_ONCE),
+				"multiclass training approach (allatonce or pairwise)")
+		(PR_SEL_TYPE, value<string>()->default_value(SEL_TYPE_PATTERN),
+				"model selection type (grid or pattern)")
+		(PR_MATRIX_TYPE, value<string>()->default_value(MAT_TYPE_SPARSE),
+				"data representation (sparse or dense)")
+		(PR_STOP_CRIT, value<string>()->default_value(STOP_CRIT_ADJMN),
+				"stopping criterion (adjmn, mn or meb)")
+		(PR_OPTIMIZATION, value<string>()->default_value(OPTIMIZATION_MDM),
+				"optimization strategy (mdm or imdm)")
+		(PR_ID_RANDOMIZER, value<string>()->default_value(ID_RANDOMIZER_FAIR),
+				"id generator (simple, fair or determ)")
+		(PR_CACHE_SIZE, value<int>()->default_value(DEFAULT_CACHE_SIZE),
+				"cache size (in MB)")
 		(PR_INPUT, value<string>(), "input file");
 
 	positional_options_description opt;
